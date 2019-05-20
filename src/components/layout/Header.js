522b6dcd379f92ac9ffaa3f0c85db4ee59875389
Reno Lazara <killerreno@gmail.com>
2019-05-20 23:42:21 +0200

    added the project

---
+++ src/components/layout/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {NavLink} from 'react-router-dom';
+
+const Header = (props) => {
+  const {branding} = props;
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+      <div className="container">
+        <NavLink exact to="/" className="navbar-brand">{branding}</NavLink>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink exact to="/contact/add" className="nav-link"><i className="fas fa-plus"/> Add contact</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink exact to="/about" className="nav-link"><i className="fas fa-question"/> About</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+};
+
+Header.defaultProps = {
+  branding: 'My App'
+};
+
+Header.propTypes = {
+  branding: PropTypes.string.isRequired
+};
+
+
+export default Header;
