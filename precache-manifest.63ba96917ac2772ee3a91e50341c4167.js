814972c5073dae1a6bcd9b400b53d63fbbd8afba
Reno Lazara <killerreno@gmail.com>
2019-05-21 14:00:08 +0200

    Updates

---
+++ precache-manifest.63ba96917ac2772ee3a91e50341c4167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b2cd7cebb09957c4773",
+    "url": "/contactmanager_pages/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "2b2cd7cebb09957c4773",
+    "url": "/contactmanager_pages/static/js/main.4b0deff0.chunk.js"
+  },
+  {
+    "revision": "62d76d8cdd4b2a560b5c",
+    "url": "/contactmanager_pages/static/js/runtime~main.5ed0fc75.js"
+  },
+  {
+    "revision": "cf25545ea437590d48db",
+    "url": "/contactmanager_pages/static/css/2.0a5e90f5.chunk.css"
+  },
+  {
+    "revision": "cf25545ea437590d48db",
+    "url": "/contactmanager_pages/static/js/2.76c3b113.chunk.js"
+  },
+  {
+    "revision": "f162e4c93c310d44201094ac95de44d5",
+    "url": "/contactmanager_pages/index.html"
+  }
+];
